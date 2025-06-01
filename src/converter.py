87b0362d526155229f2bae8833c9db29c1fd9f63
artem <artem.salikhov.04@inbox.ru>
2025-06-01 13:36:39 +0300

    Ревью кода

--- src/converter.py
+++ src/converter.py
@@ -1,12 +1,16 @@
 # src/converter.py
 
+KM_TO_MILES_RATIO = 0.621371
+
 def km_to_miles(km):
+    """Convert kilometers to miles."""
     if km < 0:
         raise ValueError("Distance cannot be negative")
-    return km * 0.621371
+    return km * KM_TO_MILES_RATIO
 
 
 def miles_to_km(miles):
+    """Convert miles to kilometers."""
     if miles < 0:
         raise ValueError("Distance cannot be negative")
-    return miles * 1.60934
+    return miles / KM_TO_MILES_RATIO
